6c31dc6e8e00d64d7eb756645ae8f19c1f97aeb7
Casper Ndlovu <casperndlovu42@gmail.com>
2025-06-02 15:57:19 +0200

    fix: integrate pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
       {
         name: "pitcowebapp",
         script: "npx expo start --web",
+        // args: "build 8081 --spa",
         env_production: {
           NODE_ENV: "production",
         },
